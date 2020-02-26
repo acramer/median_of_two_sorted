22b1df8bb7751d81171d203b17c2baa4e5714bb8
Andrew <acramerxc@utexas.edu>
2020-02-25 19:23:31 -0600

    Median Calculator Pseudo-Code Added and partially implemented

--- Median_Calculator.py
+++ Median_Calculator.py
@@ -38,10 +38,35 @@ def median_eval(i: List[int], j: List[int]) -> float:
         return (i[len(i)//2]+i[len(i)//2 - 1])/2.0 
     else:
         return i[len(i)//2]/1.0
+    # Faster Version
+    # Loop:
+    while len(bucketOne) == 1 or len(bucketTwo) == 1:
+    # 1. Divide Buckets in 2. Defaults to having larger half in the first half
+        bucketOne = [bucketOne[(len(bucketOne)+1)//2], bucketOne[(len(bucketOne)+1)//2]]
+        bucketTwo = [bucketTwo[(len(bucketTwo)+1)//2], bucketTwo[(len(bucketTwo)+1)//2]]
+    # 2. Find interior edges that correspond to the 4 sub-buckets
+        interiors = sorted(
+                        list(
+                            zip(
+                                [bucketOne[0][-1], bucketOne[1][0] ,bucketTwo[0][-1], bucketTwo[1][0]],
+                                range(4)))
+                        , key=lambda x:x[0])
+
+    # 3. Remove exterior sub-buckets
+    #   *SEPARATE ALGORITHM*
+    #   - Keep track of # removed from each super bucket
+    # 4. Balence removals from each side using the side with the larger removal
+    #   *SEPARATE ALGORITHM*
+    #   a. if removal fails, break loop and follow base case (c)
+    # 5. Check Base Cases
+    #   a. [i][i]
+    #   b. [ii][i]
+    #   - If not in base case REPEAT
+    #   c. Failed removal balance
+    # Exit Loop
+    # 6. Follow
 
 
-def binaryMove(l: int, r:int) -> int:
-    return -1
 
 
 def median_print(w: IO[str], i: List[int], j: List[int], v: int) -> None:
